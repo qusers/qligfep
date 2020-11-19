bc62e1f70854c24f7f0b3a6ffaf634fb47a7bdb7
jesperswillem <jesperswillem@gmail.com>
2020-11-19 16:09:37 +0100

    Adding updates to generation scripts for ligand parameters.

--- scripts/generate_opls.py
+++ scripts/generate_opls.py
@@ -5,12 +5,12 @@ import os
 
 curdir = os.getcwd()
 os.chdir(curdir)
-opls2Q = '/home/1kb431/software/qligfep/opls2Q.py'
+opls2Q = '/home/wjespers/software/qligfep/opls2Q.py'
 generate = 'python3 ' + opls2Q
 
 
 for pdb in glob.glob('*.pdb'):
     name = pdb.split('.')[0]
-    options = ' -l ' + name + ' -FF OPLS2015 -o Q -m'
+    options = ' -l ' + name + ' -FF OPLS2015 -m'
     args = shlex.split(generate + options)
     out = check_output(args)
--- settings.py
+++ settings.py
@@ -13,7 +13,7 @@ Q_DIR = {'CSB':'/home/jespers/software/q6/bin/',
 BIN = os.path.join(ROOT_DIR, "bin")
 #SCHROD_DIR = '/opt/schrodinger/suites2017-3/'
 #SCHROD_DIR = '/home/apps/schrodinger2017/'
-SCHROD_DIR = '/home/jespers/software/schrodinger/'
+SCHROD_DIR = '/rmvw9010_software_home/schrodinger/latest/'
 
 # CLUSTER INPUTS. To add your own cluster, use the same input as below
 CSB = {'NODES'        : '1',
