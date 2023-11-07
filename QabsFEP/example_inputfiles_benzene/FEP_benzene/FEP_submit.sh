fb51e9efb2a85ab100268a158d38189c596f66b1
Yannickrvd <yannickrickyvandijk@gmail.com>
2023-11-07 15:32:28 +0100

    updated inputfiles

---
+++ QabsFEP/example_inputfiles_benzene/FEP_benzene/FEP_submit.sh
@@ -0,0 +1,29 @@
+#! /bin/bash
+
+temperatures=(298)
+runs=10
+restartfile=md_0000_1000.re
+workdir="$( cd -P "$( dirname "$SOURCE" )" && pwd )"
+inputefiles_fep1=$workdir/inputfiles1
+inputefiles_fep2=$workdir/inputfiles2
+inputefiles_fep3=$workdir/inputfiles3
+inputefiles_fep4=$workdir/inputfiles4
+inputefiles_fep5=$workdir/inputfiles5
+for inputfiles in "$inputefiles_fep1" "$inputefiles_fep2" "$inputefiles_fep3" "$inputefiles_fep4" "$inputefiles_fep5"; do
+submitfile=$inputfiles/runALICE.sh
+sed -i s/finalMDrestart=.*/finalMDrestart="$restartfile"/g $submitfile
+sed -i s#workdir=.*#workdir="$workdir"#g $submitfile
+sed -i s#inputfiles=.*#inputfiles="$inputfiles"#g $submitfile
+done
+
+for inputfiles in "$inputefiles_fep1" "$inputefiles_fep2" "$inputefiles_fep3" "$inputefiles_fep4" "$inputefiles_fep5"; do
+submitfile=$inputfiles/runALICE.sh
+for temp in ${temperatures[*]};do
+sed -i s/temperature=.*/temperature="$temp"/g $submitfile
+for i in $(seq 1 $runs);do
+sed -i s/run=.*/run="$i"/g $submitfile
+sbatch $submitfile
+done
+done
+wait
+done
