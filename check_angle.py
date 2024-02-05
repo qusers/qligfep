48682c4d319842b0a043230a255cfd6b499a2038
Yannickrvd <yannickrickyvandijk@gmail.com>
2024-02-05 12:17:53 +0100

    updated

--- check_angle.py
+++ check_angle.py
@@ -14,13 +14,14 @@ from pymol import cmd
 class Run(object):
     """
     """
-    def __init__(self, FEP, INPUTS, FF, *args, **kwargs):
+    def __init__(self, FEP, INPUTS, FF, convert_files, *args, **kwargs):
         self.FEP = FEP.strip('/')
         self.INPUTS = INPUTS.strip('/')
-        self.dG_BAR = []
-        self.sampling = []
+        self.dihedralangle1 = []
+        self.dihedralangle2 = []
         self.rootdir = os.getcwd() + "/"
         self.FF = FF
+        self.convert_files = convert_files
 
     def replace(self, string, replacements):
         pattern = re.compile(r'\b(' + '|'.join(replacements.keys()) + r')\b')
@@ -47,28 +48,29 @@ class Run(object):
         replacements['LIB_FILE_PATH'] = libfile_file
         replacements["FF_FILE_PATH"] = self.rootdir + 'FF/' + self.FF + '.lib'
         for folder in directories:
-            print(folder)
             qfepfilesrep = sorted(glob.glob(folder + '/*.re'))
             qfepfilesrep = [file_ for file_ in qfepfilesrep if "md"in file_]
             qfepfilesrep = sorted(qfepfilesrep)
             for file in qfepfilesrep:
-                print(file)
-                shutil.copy("INPUTS/convert_re2pdb.inp", self.rootdir + folder)
-                full_path = self.rootdir + file
-                replacements['RE_INPUT'] = file.split("/")[-1]
-                replacements['PDB_OUPUT'] = file.split("/")[-1].strip('.re') + ".pdb"
-                run.replace_placeholders(replacements, self.rootdir + folder + "/convert_re2pdb.inp",  self.rootdir + folder + "/convert_re2pdb_temp.inp")
-                writedirectory = self.rootdir + folder
-                run.qprep(writedirectory)
-                print(file)
+                if self.convert_files == True:
+                    shutil.copy("INPUTS/convert_re2pdb.inp", self.rootdir + folder)
+                    full_path = self.rootdir + file
+                    replacements['RE_INPUT'] = file.split("/")[-1]
+                    replacements['PDB_OUPUT'] = file.split("/")[-1].strip('.re') + ".pdb"
+                    run.replace_placeholders(replacements, self.rootdir + folder + "/convert_re2pdb.inp",  self.rootdir + folder + "/convert_re2pdb_temp.inp")
+                    writedirectory = self.rootdir + folder
+                    run.qprep(writedirectory)
                 filepath_created_pdb = file.strip('.re') + ".pdb"
                 cmd.load(filepath_created_pdb,"ethylbenzene")
-            #break
         no_of_states = cmd.count_states("ethylbenzene")
         print(no_of_states)
         for state_num in range(1, no_of_states + 1):
+            # first dihedral
             print(cmd.get_dihedral(atom1="resi 111 and name N",atom2="resi 111 and name CA",atom3="resi 111 and name CB",atom4="resi 111 and name CG1",state=state_num))
 
+            # second dihedral
+            #print(cmd.get_dihedral(atom1="resi 111 and name N",atom2="resi 111 and name CA",atom3="resi 111 and name CB",atom4="resi 111 and name CG2",state=state_num))
+    
     def replace_placeholders(self, replacements, convertion_Q_file_path, convertion_Q_output):
         with open(convertion_Q_file_path) as infile, open(convertion_Q_output, 'w') as outfile:
             for line in infile:
@@ -97,10 +99,17 @@ if __name__ == "__main__":
                         choices = ['OPLS2005', 'OPLS2015', 'AMBER14sb', 'CHARMM36', 'CHARMM22', 'CHARMM_TEST'],
                         help = "Forcefield to be used")
 
+    parser.add_argument('-convert', '--convert_files',
+                        dest = "convert_files",
+                        required = False,
+                        action='store_true',
+                        help = "Convert .re to .pdb files")
+
     args = parser.parse_args()
     run = Run(FEP = args.FEP,
               INPUTS = args.INPUTS,
-              FF = args.FF)
+              FF = args.FF,
+              convert_files = args.convert_files)
 
     run.make_pdb_files()
     
